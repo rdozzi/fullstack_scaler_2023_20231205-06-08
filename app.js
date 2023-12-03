3e0ac7b6ab1da25da7f77a63a55320b006c05668
Raphael Dozzi <rdozzi84@gmail.com>
2023-12-03 15:25:22 -0800

    Created Schema with Validation

--- app.js
+++ app.js
@@ -34,6 +34,44 @@ app.use((req,res,next) => {
     next()
 })
 
+/** Schemas */
+const userSchema = new mongoose.Schema({
+    // Initially we can define these items with a string but we want to add validation checks. Use an object
+    // name:String,
+    name:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        requires:true,
+        unique:true
+    },
+    phone:{
+        type:Number,
+        required:true,
+        unique:true,
+        minlength:10
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:8
+    },
+    confirmPassword:{
+        type:String,
+        required:true,
+        minlength:8,
+        validate: {
+            validator: function(){
+            return this.password === this.confirmPassword
+            },
+            message: "Password and Confirm Password should be same."
+        }
+    }
+})
+
 /** Route Handlers */
 
 function getUserHandler(req,res){
