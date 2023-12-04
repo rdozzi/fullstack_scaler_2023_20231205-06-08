9e48276bc74741a73b3bda672ef538ef5a71e251
Raphael Dozzi <rdozzi84@gmail.com>
2023-12-03 19:07:10 -0800

    Added patch updated handler

--- app.js
+++ app.js
@@ -142,13 +142,26 @@ async function getUserByIdHandler (req,res){
 }
 
 // Recall that interactions are always asynchronous and we need to wait for them. That's why we need async
-// async function updateUserByIdHandler(req,res){
-//     try{
-//         const {id} = req.params;
-//         const userDetails = req.body;
-//         const updatedUser
-//     }
-// }
+async function updateUserByIdHandler(req,res){
+    try{
+        const {id} = req.params;
+        const userDetails = req.body;
+        const updatedUser = await User.findByIdAndUpdate(id,userDetails,{new: true}) // {new: true} returns object/document after its updated
+        if(!updatedUser){
+            throw new error('No User Found!')
+        }else {
+            res.status(200).json({
+                message: "User was updated successfully",
+                data: updatedUser
+            });
+        }
+    }catch(err){
+        res.status(500).json({
+            message: "error",
+            data: err.message
+        });
+    }
+}
 
 /** Routes */
 
@@ -161,6 +174,6 @@ app.post('/api/users', createUserHandler)
 app.get('/api/users/:id', getUserByIdHandler)
 
 // We patch instead of put because we want to update only one entry vs the entire object
-// app.patch("/api/users/:id", updateUserByIdHandler)
+app.patch("/api/users/:id", updateUserByIdHandler)
 
 app.listen(process.env.PORT, () => console.log(`Listening at ${process.env.PORT}`))
