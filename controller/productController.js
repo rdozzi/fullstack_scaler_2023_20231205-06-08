86fa08d27c52e6439e004730f5537c523a3926ec
Raphael Dozzi <rdozzi84@gmail.com>
2023-12-10 10:58:02 -0800

    Refactored productController with crudFactory reference methods

--- controller/productController.js
+++ controller/productController.js
@@ -1,125 +1,143 @@
 const Product = require('../models/productModel')
+const {checkInput, 
+    getAllFactory, 
+    createFactory, 
+    getElementByIdFactory,
+    updateElementByIdFactory,
+    deleteElementsByIdFactory} = require("../utils/crudFactory")
 
-const checkInput = function (req,res,next){
-    const productDetails = req.body;
-    const isEmpty = Object.keys(productDetails).length === 0
-    if(isEmpty){
-        res.status(400).json({
-            message: "error",
-            data: "Input fields cannot be empty"
-        })
-    } else {
-        next();
-    }
-}
+// This will replicate the changes to userController
+
+// const checkInput = function (req,res,next){
+//     const productDetails = req.body;
+//     const isEmpty = Object.keys(productDetails).length === 0
+//     if(isEmpty){
+//         res.status(400).json({
+//             message: "error",
+//             data: "Input fields cannot be empty"
+//         })
+//     } else {
+//         next();
+//     }
+// }
 
 /** Route Handlers */
 
-async function getProductHandler(req,res){
-    try{
-        const productData = await Product.find()
-        if(productData.length === 0){
-            throw new Error('No users found')
-        } else {
-            res.status(200).json({
-                message: 'success',
-                data: productData
-            })
-        }
+const getProductHandler = getAllFactory(Product)
 
-    }catch(err){
-        res.status(500).json({
-            message: 'error',
-            data: err.message
-        })
+// async function getProductHandler(req,res){
+//     try{
+//         const productData = await Product.find()
+//         if(productData.length === 0){
+//             throw new Error('No users found')
+//         } else {
+//             res.status(200).json({
+//                 message: 'success',
+//                 data: productData
+//             })
+//         }
 
-    }
-}
+//     }catch(err){
+//         res.status(500).json({
+//             message: 'error',
+//             data: err.message
+//         })
+
+//     }
+// }
 
 // Update the function to an async function to call product information from the db
-// 
-async function createProductHandler(req,res){
-    try{
 
-        const productDetails = req.body
-        const product = await Product.create(productDetails)
-        res.status(201).json({
-            message:"product was created successfully",
-            data:product
-        })
+const createProductHandler = createFactory(Product)
 
-    } catch(err){
-        res.status(500).json({
-            message: "error",
-            data: err.message
-        })
-    }
-}
+// async function createProductHandler(req,res){
+//     try{
 
-async function getProductByIdHandler(req,res){
-    console.log(req.params)
-    const {id} = req.params //Deconstruction
-    try{
+//         const productDetails = req.body
+//         const product = await Product.create(productDetails)
+//         res.status(201).json({
+//             message:"product was created successfully",
+//             data:product
+//         })
+
+//     } catch(err){
+//         res.status(500).json({
+//             message: "error",
+//             data: err.message
+//         })
+//     }
+// }
+
+const getProductByIdHandler = getElementByIdFactory(Product)
+
+// async function getProductByIdHandler(req,res){
+//     console.log(req.params)
+//     const {id} = req.params //Deconstruction
+//     try{
     
-        const product = await Product.findById(id)
-        if(!product){
-            throw new Error('No product found!')
-        } else {
-            res.status(200).json({
-                message: "success",
-                data: product,
-            })
-        }
+//         const product = await Product.findById(id)
+//         if(!product){
+//             throw new Error('No product found!')
+//         } else {
+//             res.status(200).json({
+//                 message: "success",
+//                 data: product,
+//             })
+//         }
 
-    }catch(err){
-        res.status(500).json({
-            message: "error",
-            data: err.message
-        })
-    }
-}
+//     }catch(err){
+//         res.status(500).json({
+//             message: "error",
+//             data: err.message
+//         })
+//     }
+// }
+
+const updateProductByIdHandler = updateElementByIdFactory(Product)
 
 // Recall that interactions are always asynchronous and we need to wait for them. That's why we need async
-async function updateProductByIdHandler(req,res){
-    try{
-        const {id} = req.params;
-        const productDetails = req.body;
-        const updatedProduct = await Product.findByIdAndUpdate(id,productDetails,{new: true}) // {new: true} returns object/document after its updated
-        if(!updatedProduct){
-            throw new error('No Product Found!')
-        }else {
-            res.status(200).json({
-                message: "Product was updated successfully",
-                data: updatedProduct
-            });
-        }
-    }catch(err){
-        res.status(500).json({
-            message: "error",
-            data: err.message
-        });
-    }
-}
+// async function updateProductByIdHandler(req,res){
+//     try{
+//         const {id} = req.params;
+//         const productDetails = req.body;
+//         const updatedProduct = await Product.findByIdAndUpdate(id,productDetails,{new: true}) // {new: true} returns object/document after its updated
+//         if(!updatedProduct){
+//             throw new error('No Product Found!')
+//         }else {
+//             res.status(200).json({
+//                 message: "Product was updated successfully",
+//                 data: updatedProduct
+//             });
+//         }
+//     }catch(err){
+//         res.status(500).json({
+//             message: "error",
+//             data: err.message
+//         });
+//     }
+// }
 
-async function deleteProductsByIdHandler(req,res){
-    try{
-        const {id} = req.params;
-        const deletedProduct = await Product.findByIdAndDelete(id);
-        if(!deletedProduct){
-            throw new error('No Product Found!')
-        }else {
-            res.status(200).json({
-                message: "Product was deleted successfully",
-                data: deletedProduct
-            });
-        }
-    }catch(err){
-        res.status(500).json({
-            message: "error",
-            data: err.message
-        });
-    }
-}
+const deleteProductsByIdHandler = deleteElementsByIdFactory(Product)
+
+// async function deleteProductsByIdHandler(req,res){
+//     try{
+//         const {id} = req.params;
+//         const deletedProduct = await Product.findByIdAndDelete(id);
+//         if(!deletedProduct){
+//             throw new error('No Product Found!')
+//         }else {
+//             res.status(200).json({
+//                 message: "Product was deleted successfully",
+//                 data: deletedProduct
+//             });
+//         }
+//     }catch(err){
+//         res.status(500).json({
+//             message: "error",
+//             data: err.message
+//         });
+//     }
+// }
 
 module.exports = {
     getProductHandler,
