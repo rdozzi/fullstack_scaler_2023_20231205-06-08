cf91a708ac9fb662f7f5493f3fcd58fde6074a67
Raphael Dozzi <rdozzi84@gmail.com>
2023-12-17 09:42:35 -0800

    Added transporter method with .env entry

--- nodemailer.js
+++ nodemailer.js
@@ -1,1 +1,15 @@
 const nodemailer = require("nodemailer")
+require("dotenv").config()
+
+// Create Transporter Object
+
+const transporter = nodemailer.createTransport({
+    service:"gmail",
+    host:"smtp.gmail.com",
+    port:587,
+    secure:false, // Initialize as false. As you connect, it will upgrade.
+    auth:{
+        user: "rdozzi84@gmail.com",
+        pass: process.env.GOOGLE_PASSWORD
+    }
+})
