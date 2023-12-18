7d03b15db33e741e522249ee8cbe7c13892dcc96
Raphael Dozzi <rdozzi84@gmail.com>
2023-12-17 21:46:36 -0800

    Refactored model and added pre hook

--- models/userModel.js
+++ models/userModel.js
@@ -44,15 +44,29 @@ const userSchema = new mongoose.Schema({
             message: "Password and Confirm Password should be same."
         }
     },
-    role:{
-        type:String
-    },
     token:String,
     otpExpiry:Date,
-})
+    role:{
+        type:String,
+        default:"user"
+    },
+});
 
-userSchema.pre("save",function(){
-    this.confirmPassword = undefined
+const validRoles = ["admin","user","sales"];
+
+userSchema.pre("save",function(next){
+    this.confirmPassword = undefined;
+    if(this.role){
+        console.log(this.role);
+        const isValid = validRoles.includes(this.role);
+        if(!isValid){
+            throw new Error(`Invalid role ${this.role}`);
+        }else{
+            next()
+        }
+    }else{
+        this.role = "user";
+    }
 })
 
 /** Models */
