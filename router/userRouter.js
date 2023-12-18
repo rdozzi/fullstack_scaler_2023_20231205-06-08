107d2607e9c846ed065b1f625ef22ffef2cac59b
Raphael Dozzi <rdozzi84@gmail.com>
2023-12-17 21:07:33 -0800

    Removed auth/obsolete routers fom userRouter

--- router/userRouter.js
+++ router/userRouter.js
@@ -20,11 +20,6 @@ const {checkInput} = require("../utils/crudFactory")
 
 /** User Routes */
 userRouter.get('/allUsers', protectRoute, isAdmin, getUserHandler)
-userRouter.post('/signup', checkInput, signupHandler)
-userRouter.post('/login', checkInput,loginHandler)
-userRouter.post('/', checkInput, createUserHandler) //To Delete
-userRouter.post('/forgetPassword',forgetPassword)
-userRouter.patch('/resetPassword/:userId',resetPassword)
 userRouter.get('/:id',getUserByIdHandler)
 userRouter.patch('/:id',updateUserByIdHandler)
 userRouter.delete('/:id',deleteUserByIdHandler)
